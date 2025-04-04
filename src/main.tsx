8f60d0a82c619a5ade77b66b74e4f43cf5ced2d0
arun <arunraval3232@gmail.com>
2025-04-04 04:15:06 -0700

    add change for the file

--- src/main.tsx
+++ src/main.tsx
@@ -9,16 +9,16 @@ import Header from "./components/header/Header.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <Provider store={store}>
+    <Provider store={store}>
       <div className="layout-container">
         <Sidebar />
         <div className="main-content">
           <Header />
-          <div className="content-wrapper"> */}
-    <App />
-    {/* </div> */}
-    {/* </div> */}
-    {/* </div> */}
-    {/* </Provider> */}
+          <div className="content-wrapper">
+            <App />
+          </div>
+        </div>
+      </div>
+    </Provider>
   </StrictMode>
 );
